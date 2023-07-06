d8bd924e71c7293b0faf423f11f980974c07e641
YeonsangYoon <1004dustkd@gmail.com>
2023-07-06 09:01:52 +0900

    apply lombok

--- src/main/java/com/sist/vo/BoardVO.java
+++ src/main/java/com/sist/vo/BoardVO.java
@@ -2,16 +2,17 @@ package com.sist.vo;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class BoardVO {
 	//  
 	private int no, hit, board_id, comment_index, comment_tab, comment_cnt;
 	private String name, title, content, category;
 	private Date regdate;
-	
-	
-	
-	
-	
 }
 
 
