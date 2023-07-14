80f4fec089afdd9b35d2b5cd9b13f52f90058046
Yun Jisoo <yjs49420206@gmail.com>
2023-07-14 18:18:46 +0900

    ReserveVO 추가

--- src/main/java/com/sist/vo/ReserveVO.java
+++ src/main/java/com/sist/vo/ReserveVO.java
@@ -2,11 +2,13 @@ package com.sist.vo;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import java.util.*;
 @Getter
 @Setter
 
 // 예약 정보
 public class ReserveVO {
-	
+	private String res_state,res_msg,res_img,user_id;
+	private int res_id,com_id,pet_id;
+	private Date res_date;
 }
