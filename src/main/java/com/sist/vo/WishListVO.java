ba84a5f3b752214bb161a32118ae37c371a6e9fe
Yun Jisoo <yjs49420206@gmail.com>
2023-07-18 15:01:08 +0900

    mypage info경로추가, wishlist추가

--- src/main/java/com/sist/vo/WishListVO.java
+++ src/main/java/com/sist/vo/WishListVO.java
@@ -3,33 +3,10 @@ package com.sist.vo;
 import lombok.Getter;
 import lombok.Setter;
 
-/*
- 	wno: 위시리스트 번호 wish_no
- 	cno: 업체 번호
- 	uid: user_id
- 	cname, cposter: 업체이름,업체사진
- 	ccategory:업체카테고리번호
- 	
-   --wishcompany--	
-   com_id
-   user_id
-   wish_no
-   
-   --member--
-   user_id
-   
-  --company--
-  com_id
-  com_category_id
-  com_name
-  poster
-   
-
- */
 @Getter
 @Setter
 
 public class WishListVO {
 	private int com_id,com_category_id;
-	private String com_name,poster,user_id;
+	private String com_name,poster,user_id,category;
 }
