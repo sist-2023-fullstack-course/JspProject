bb758a8c71532c381970d2f26d9a00802588fa3e
GyulCode <ldg2057@naver.com>
2023-07-21 00:42:28 +0900

    vo 업데이트

--- src/main/java/com/sist/vo/BoardVO.java
+++ src/main/java/com/sist/vo/BoardVO.java
@@ -15,74 +15,7 @@ public class BoardVO {
 	private int board_id, hit, rownum, comment_cnt;
     private String title, content, user_id, user_name, dbday, board_category;
     
-	public String getUser_name() {
-		return user_name;
-	}
-	public void setUser_name(String user_name) {
-		this.user_name = user_name;
-	}
-	public int getBoard_id() {
-		return board_id;
-	}
-	public void setBoard_id(int board_id) {
-		this.board_id = board_id;
-	}
-	public int getHit() {
-		return hit;
-	}
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
-	public int getRownum() {
-		return rownum;
-	}
-	public void setRownum(int rownum) {
-		this.rownum = rownum;
-	}
-	public int getComment_cnt() {
-		return comment_cnt;
-	}
-	public void setComment_cnt(int comment_cnt) {
-		this.comment_cnt = comment_cnt;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-	public String getDbday() {
-		return dbday;
-	}
-	public void setDbday(String dbday) {
-		this.dbday = dbday;
-	}
-	public String getBoard_category() {
-		return board_category;
-	}
-	public void setBoard_category(String board_category) {
-		this.board_category = board_category;
-	}
-    
-    //private Date regdate;
-    
-    
-    
-    
-    
-    
+	
     
 }
 
